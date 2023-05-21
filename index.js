8a02978c62e07b7742bae98ff3de3cf51c9e1924
Victor <victorvergarareyes2002@gmail.com>
2023-05-21 11:02:00 -0600

    middleware

--- index.js
+++ index.js
@@ -1,57 +1,31 @@
-// const http=require("http")
-// const fs=require("fs")
-
-// const server=http.createServer((req,res)=>{
-//     const read=fs.createReadStream("./static/index.html")
-//     read.pipe(res)
-// })
-
-// server.listen(3000)
-// console("server on port 3000");
-
 const express=require("express")
+const morgan=require("morgan")
 
 const app=express()
 
-app.get("/",(req,res)=>{
-    res.send("Hello world")
+app.use(morgan('dev'))
+
+app.use((req,res,next)=>{
+    console.log('paso por aqui')
+
+    next()
 })
 
-app.get("/imagen",(req,res)=>{
-    res.sendFile("./static/img/bote de pintura.png",{
-        root:__dirname
-    })
+app.use((req,res,next)=>{
+    if(req.query.login=="vicvergara@gmail.com"){
+        next()
+    }else{
+        res.send("No autorizado")
+    }
 })
 
-// app.get("/user",(req,res)=>{
-//     res.json({"name":"victor"})})
-
-app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
-app.post('/user',(req,res)=>{
-    console.log(req.body)
-    res.send("Nuevo usuario creado")
+app.get("/dashboard",(req,res)=>{
+    res.send("Dashboard page")
 })
 
-app.get('/hello/:username',(req,res)=>{
-    console.log(req.params.username)
-    res.send(`HELLO ${req.params.username.toUpperCase()}`)
-})
-
-
-app.get("/add/:x/:y",(req,res)=>{
-    console.log(req.params.x)
-    console.log(req.params.y)
-    result=parseInt(req.params.x)+parseInt(req.params.y)
-    res.send(`Result ${result}`)
+app.get("/profile",(req,res)=>{
+    res.send("profile page")
 })
 
 app.listen(3000)
 console.log("server on port 3000")
-
-app.use((req,res)=>{
-    res.status(404).send("No se encontro, error 404");
-})
-
